3d1c20c044a18fdc85f79b781c6c4fa40521da59
Lewis Jang <lewis.aedes@gmail.com>
2025-04-13 19:55:24 +0800

    fixed get api bugs and update api to all model

--- controllers/main.py
+++ controllers/main.py
@@ -6,11 +6,11 @@ import json
 
 class Regedit(http.Controller):
 
-    @http.route('/audio/get/<int:object_id>', type='http', auth='public', methods=['GET'])
-    def audio_file_get(self, object_id, **get):
+    @http.route('/api/<string:model_name>/get/<int:object_id>', type='http', auth='public', methods=['GET'])
+    def audio_file_get(self, model_name, object_id, **get):
         request.session.authenticate(get['db'], login=get['username'], password=get['password'])
 
-        obj = request.env['audio.file'].sudo().browse(object_id)
+        obj = request.env[model_name].sudo().browse(object_id)
 
         if not obj.exists():
             return Response(
@@ -22,7 +22,7 @@ class Regedit(http.Controller):
 
         for field_index, field_object in obj._fields.items():
             field_value = getattr(obj, field_index)
-            data[field_index] = field_index
+            data[field_index] = field_value
 
             if field_object.type == "many2many":
                 data[field_index] = [related.id for related in field_value]
@@ -33,11 +33,11 @@ class Regedit(http.Controller):
         return Response(json.dumps({"respond": data}), status=200)
 
 
-    @http.route('/audio/set/<int:object_id>', type='http', auth='public', methods=['GET'])
-    def audio_set_get(self, object_id, **get):
+    @http.route('/api/<string:model_name>/set/<int:object_id>', type='http', auth='public', methods=['GET'])
+    def audio_set_get(self, model_name, object_id, **get):
         request.session.authenticate(get['db'], login=get['username'], password=get['password'])
 
-        obj = request.env['audio.file'].sudo().browse(object_id)
+        obj = request.env[model_name].sudo().browse(object_id)
 
         if not obj.exists():
             return {"respond": "404"}
